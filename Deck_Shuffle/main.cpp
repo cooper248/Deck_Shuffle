892ff38d7545a7776362cfee66ea4c16bb39f694
Cooper Richardson <cooperrichardson@Coopers-MacBook-Pro.local>
2017-02-05 22:11:47 -0800

    Initial Commit

---
+++ Deck_Shuffle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Deck_Shuffle
+//
+//  Created by Cooper Richardson on 2/5/17.
+//  Copyright © 2017 Cooper Richardson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
